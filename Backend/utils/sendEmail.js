5646102130182bc1d12ec246970e0049629238e3
Emmanueladimchinaobi <deborahamybliss@gmail.com>
2026-07-17 05:20:54 +0100

    Add email logging

--- Backend/utils/sendEmail.js
+++ Backend/utils/sendEmail.js
@@ -1,5 +1,4 @@
 const { Resend } = require("resend");
-console.log("RESEND KEY:", process.env.RESEND_API_KEY);
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
@@ -7,11 +6,55 @@ const sendOrderEmail = async (order) => {
     console.log("📨 Sending email for order:", order.customerName);
 
     try {
+
+        const items = order.items.map(item =>
+            `• ${item.name}
+   Size: ${item.size}
+   Quantity: ${item.quantity}
+   Price: ₦${item.price.toLocaleString()}`
+        ).join("\n\n");
+
         const result = await resend.emails.send({
             from: "onboarding@resend.dev",
             to: process.env.OWNER_EMAIL,
             subject: "🛒 New Jersey Store Order",
-            text: `New order from ${order.customerName}`
+            text: `
+NEW ORDER RECEIVED
+
+Customer:
+${order.customerName}
+
+Phone:
+${order.phone}
+
+Address:
+${order.address}
+
+----------------------------------
+
+ITEMS
+
+${items}
+
+----------------------------------
+
+Total:
+₦${order.total.toLocaleString()}
+
+Payment Method:
+${order.paymentMethod}
+
+Payment Status:
+${order.paymentStatus || "Pending"}
+
+Order Status:
+${order.orderStatus || "Pending"}
+
+----------------------------------
+
+Order Time:
+${new Date(order.createdAt || Date.now()).toLocaleString()}
+`
         });
 
         console.log("✅ Resend response:", result);
