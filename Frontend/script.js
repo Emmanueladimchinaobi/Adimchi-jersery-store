bb995fde34e8bc4d0eaf28d11a88b359013f6429
Emmanueladimchinaobi <deborahamybliss@gmail.com>
2026-08-11 17:52:30 +0100

    first commit

--- Frontend/script.js
+++ Frontend/script.js
@@ -3,13 +3,21 @@ const closeBtn = document.getElementById("closeBtn");
 const mobileMenu = document.getElementById("mobileMenu");
 
 menuBtn.addEventListener("click", () => {
-  mobileMenu.classList.remove("hidden");
-  mobileMenu.classList.add("flex");
+    mobileMenu.classList.remove("-translate-x-full");
+    mobileMenu.classList.add("-translate-x-0");
+
+    mobileMenu.classList.remove("opacity-0");
+
+    mobileMenu.classList.remove("pointer-events-none");
 });
 
 closeBtn.addEventListener("click", () => {
-  mobileMenu.classList.remove("flex");
-  mobileMenu.classList.add("hidden");
+    mobileMenu.classList.remove("-translate-x-0");
+    mobileMenu.classList.add("-translate-x-full");
+
+    mobileMenu.classList.add("opacity-0");
+
+    mobileMenu.classList.add("pointer-events-none");
 });
  const Clubbtn = document.getElementById("Club-btn");
  const Clubmenu = document.getElementById("Club-menu");
@@ -43,69 +51,62 @@ document.addEventListener("click", (e) => {
 });
 
 
-const shopbtn = document.getElementById("shop-btn");
- const shopmenu = document.getElementById("shop-menu");
- shopbtn.addEventListener("click" , () => {
-    shopmenu.classList.toggle("hidden");
- });
 
- // Close the dropdown when clicking anywhere else
-document.addEventListener("click", (e) => {
-    if (
-        !shopbtn.contains(e.target) &&
-        !shopmenu.contains(e.target)
-    ) {
-        shopmenu.classList.add("hidden");
-    }
-});
  
-const clubsbtn2 = document.getElementById("clubs-btn2");
-const backbtn = document.getElementById("back-btn");
-
-const mainMenu = document.getElementById("main-menu");
+const clubsbtn2 = document.getElementById("clubsbtn2");
+const backbtn = document.getElementById("backbtn");
 const submenu = document.getElementById("submenu");
 
 // Open submenu
 clubsbtn2.addEventListener("click", () => {
-  mainMenu.classList.remove("translate-x-0");
-  mainMenu.classList.add("-translate-x-full");
+ 
+  
 
-  submenu.classList.remove("translate-x-full");
-  submenu.classList.add("translate-x-0");
+  submenu.classList.remove("-translate-x-full");
+  submenu.classList.add("-translate-x-0");
+  submenu.classList.remove("opacity-0");
+  submenu.classList.remove("pointer-events-none");
+  
 });
 
 // Go back
 backbtn.addEventListener("click", () => {
-  submenu.classList.remove("translate-x-0");
-  submenu.classList.add("translate-x-full");
+  submenu.classList.remove("-translate-x-0");
+  submenu.classList.add("-translate-x-full");
+  submenu.classList.add("opacity-0");
+  submenu.classList.add("pointer-events-none");
+  
 
-  mainMenu.classList.remove("-translate-x-full");
-  mainMenu.classList.add("translate-x-0");
+  
+  
 });
 
-const brandsbtn = document.getElementById("brands-btn");
-const brandbackbtn = document.getElementById("brand-backbtn");
 
-const brandsmenu = document.getElementById("brands-menu");
-const brandssubmenu = document.getElementById("brands-submenu");
+const Brandbtn2 = document.getElementById("Brandbtn2");
+const brandbackbtn = document.getElementById("brandbackbtn");
+const brandsubmenu = document.getElementById("brandsubmenu");
 
-// Open submenu
-brandsbtn.addEventListener("click", () => {
-  brandsmenu.classList.remove("translate-x-0");
-  brandsmenu.classList.add("-translate-x-full");
-
-  brandssubmenu.classList.remove("translate-x-full");
-  brandssubmenu.classList.add("translate-x-0");
+Brandbtn2.addEventListener("click", () => {
+    brandsubmenu.classList.remove("-translate-x-full");
+  brandsubmenu.classList.add("-translate-x-0");
+  brandsubmenu.classList.remove("opacity-0");
+  brandsubmenu.classList.remove("pointer-events-none");
+  
+   
 });
 
-// Go back
 brandbackbtn.addEventListener("click", () => {
-  brandssubmenu.classList.remove("translate-x-0");
-  brandssubmenu.classList.add("translate-x-full");
-
-  brandsmenu.classList.remove("-translate-x-full");
-  brandsmenu.classList.add("translate-x-0");
+   brandsubmenu.classList.remove("-translate-x-full");
+  brandsubmenu.classList.add("-translate-x-0");
+  brandsubmenu.classList.add("opacity-0");
+  brandsubmenu.classList.add("pointer-events-none");
+  
 });
+
+
+
+
+
 
 
  const images = [
@@ -126,7 +127,76 @@ brandbackbtn.addEventListener("click", () => {
       currentIndex = (currentIndex + 1) % images.length;
       document.getElementById("photo").src = images[currentIndex];
     };
+//*******products */
+
+const products = document.querySelectorAll(".product");
+const pagination = document.getElementById("pagination");
 
 
+const productsPerPage = 3;
+
+let currentPage = 1;
+
+const totalPages = Math.ceil(products.length / productsPerPage);
+
+
+function showProducts(page) {
+
+    currentPage = page;
+
+    const start = (page - 1) * productsPerPage;
+    const end = start + productsPerPage;
+
+    products.forEach((product, index) => {
+
+        if (index >= start && index < end) {
+
+            product.classList.remove("hidden");
+
+        } else {
+
+            product.classList.add("hidden");
+
+        }
+
+    });
+
+    createPagination();
+
+}
+
+
+function createPagination() {
+
+    pagination.innerHTML = "";
+
+    for (let i = 1; i <= totalPages; i++) {
+
+        const button = document.createElement("button");
+
+        button.textContent = i;
+
+        button.className =
+            "px-4 py-2 mx-1 rounded bg-gray-200 hover:bg-pink-600 hover:text-white";
+
+        button.addEventListener("click", () => {
+
+            showProducts(i);
+
+        });
+
+        pagination.appendChild(button);
+
+    }
+
+}
+
+
+showProducts(1);
+
+
+
+
+  
 
 
